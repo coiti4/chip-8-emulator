a0177521c3ce51150868d9522d5a8b1246c199c2
Agustin Coitinho <coitinho10@gmail.com>
2025-02-23 18:22:38 -0300

    Definition of the emulator.

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,14 +1,38 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::collections::VecDeque;
+
+// The following are public because they are used in the main.rs file
+pub const SCREEN_WIDTH: usize = 64;
+pub const SCREEN_HEIGHT: usize = 32;
+
+const RAM_SIZE: usize = 4096; // 4KB
+const NUM_REGS: usize = 16;
+const STACK_SIZE: usize = 16;
+
+const START_ADDR: u16 = 0x200; // 512, CHIP-8 programs start at this address
+
+pub struct Emu {
+    pc: u16, // program counter
+    ram: [u8; RAM_SIZE],
+    screen: [bool; SCREEN_WIDTH * SCREEN_HEIGHT],
+    v_reg: [u8; NUM_REGS], // general purpose registers
+    i_reg: u16, // index register
+    stack: VecDeque<u16>,
+    dt: u8, // delay timer
+    st: u8, // sound timer
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Emu {
+    pub fn new() -> Self {
+        Self {
+            pc: START_ADDR,
+            ram: [0; RAM_SIZE],
+            screen: [false; SCREEN_WIDTH * SCREEN_HEIGHT],
+            v_reg: [0; NUM_REGS],
+            i_reg: 0,
+            stack: VecDeque::with_capacity(STACK_SIZE),
+            dt: 0,
+            st: 0,
+        }
     }
 }
+
