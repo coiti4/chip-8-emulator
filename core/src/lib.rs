1cbc64ddd08c2fbb4e0d4ea728eca93333e14713
Agustin Coitinho <coitinho10@gmail.com>
2025-02-24 00:00:21 -0300

    Font Sprites and reset CPU state.

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,5 +1,26 @@
 use std::collections::VecDeque;
 
+const FONTSET_SIZE: usize = 80;
+
+const FONTSET: [u8; FONTSET_SIZE] = [
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+];
+
 // The following are public because they are used in the main.rs file
 pub const SCREEN_WIDTH: usize = 64;
 pub const SCREEN_HEIGHT: usize = 32;
@@ -7,6 +28,7 @@ pub const SCREEN_HEIGHT: usize = 32;
 const RAM_SIZE: usize = 4096; // 4KB
 const NUM_REGS: usize = 16;
 const STACK_SIZE: usize = 16;
+const NUM_KEYS: usize = 16;
 
 const START_ADDR: u16 = 0x200; // 512, CHIP-8 programs start at this address
 
@@ -17,22 +39,42 @@ pub struct Emu {
     v_reg: [u8; NUM_REGS], // general purpose registers
     i_reg: u16, // index register
     stack: VecDeque<u16>,
+    keys: [bool; NUM_KEYS], // keypad
     dt: u8, // delay timer
     st: u8, // sound timer
 }
 
 impl Emu {
     pub fn new() -> Self {
-        Self {
+        let mut my_emu: Emu = Self {
             pc: START_ADDR,
             ram: [0; RAM_SIZE],
             screen: [false; SCREEN_WIDTH * SCREEN_HEIGHT],
             v_reg: [0; NUM_REGS],
             i_reg: 0,
             stack: VecDeque::with_capacity(STACK_SIZE),
+            keys: [false; NUM_KEYS],
             dt: 0,
             st: 0,
-        }
+        };
+
+        // Load fontset into memory
+        my_emu.ram[..FONTSET_SIZE].copy_from_slice(&FONTSET);
+
+        my_emu
+    }
+
+    pub fn reset(&mut self) {
+        self.pc = START_ADDR;
+        self.ram = [0; RAM_SIZE];
+        self.screen = [false; SCREEN_WIDTH * SCREEN_HEIGHT];
+        self.v_reg = [0; NUM_REGS];
+        self.i_reg = 0;
+        self.stack = VecDeque::with_capacity(STACK_SIZE);
+        self.keys = [false; NUM_KEYS];
+        self.dt = 0;
+        self.st = 0;
+        self.ram[..FONTSET_SIZE].copy_from_slice(&FONTSET);
     }
 }
 
